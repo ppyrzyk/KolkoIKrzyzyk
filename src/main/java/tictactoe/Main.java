84abed9ff05e02db055d68a13f22b66c327b0ed0
Pawel Pyrzyk <ppyrzyk@gmail.com>
2022-11-08 18:31:25 +0100

    Kolko

--- src/main/java/tictactoe/Main.java
+++ src/main/java/tictactoe/Main.java
@@ -2,11 +2,11 @@ package tictactoe;
 
 import java.util.Scanner;
 
+import static tictactoe.Board.board;
+
 public class Main {
     public static void main(String[] args) {
-    Board board = new Board();
-    Move move = new Move();
-    move.Play();
+        Move move = new Move();
+            Move.start();
     }
 }
-
--- src/main/java/tictactoe/Move.java
+++ src/main/java/tictactoe/Move.java
@@ -1,56 +1,57 @@
 package tictactoe;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
+
 public class Move extends Board {
+    static boolean Player1= true;
+    static Scanner scan = new Scanner(System.in);
+
+    public static void start(){
+        System.out.println("Player1 - what is your name?");
+        String p1Name= scan.nextLine();
+        System.out.println("Player2 - what is your name?");
+        String p2Name= scan.nextLine();
 
 
+        while(true) {
+            System.out.println("Select your move 1-9");
+            if(Player1){
+                System.out.println(p1Name +"'s turn (X)");
+            }else{
+                System.out.println(p2Name +"'s turn (O)");
+            }
 
-    static Scanner scanner = new Scanner(System.in);
+            String userInput = scan.nextLine();
+            char symbol = ' ';
+
+            if (Player1){
+                symbol = 'X';
+            }else{
+                symbol = 'O';
+            }
+
+            Player1=!Player1;
 
 
-    public static void Play() {
-        System.out.println("Select where you want to play on the grid = 1-9");
-        String userInput = scanner.nextLine();
-//        System.out.println(userInput);
-
-        switch (userInput) {
-            case "1":
-                board[0][0] = 'X';
-                break;
-            case "2":
-                board[0][1] = 'X';
-                break;
-            case "3":
-                board[0][2] = 'X';
-                break;
-            case "4":
-                board[1][0] = 'X';
-                break;
-            case "5":
-                board[1][1] = 'X';
-                break;
-            case "6":
-                board[1][2] = 'X';
-                break;
-            case "7":
-                board[2][0] = 'X';
-                break;
-            case "8":
-                board[2][1] = 'X';
-                break;
-            case "9":
-                board[2][2] = 'X';
-                break;
-            default:
-                System.out.println("Wrong move");
-
-        }
-         new Board();
+            switch (userInput) {
+                case "1" -> board[0][0] = symbol;
+                case "2" -> board[0][1] = symbol;
+                case "3" -> board[0][2] = symbol;
+                case "4" -> board[1][0] = symbol;
+                case "5"-> board[1][1] = symbol;
+                case "6" -> board[1][2] = symbol;
+                case "7" -> board[2][0] = symbol;
+                case "8" -> board[2][1] = symbol;
+                case "9" -> board[2][2] = symbol;
+                default -> System.out.println("Wrong move");
+            }
+             new Board();
 
 
     }
 
 }
+}
+
 
